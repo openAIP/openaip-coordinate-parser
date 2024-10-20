d455b8ea916df30c4e55db250abc5280a4e9c111
Stephan Besser <stephan.besser@gmail.com>
2024-10-20 20:05:52 +0200

    more tests
    Signed-off-by: Stephan Besser <stephan.besser@gmail.com>

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -9,12 +9,12 @@ it('throws an error if options is not an object', () => {
     expect(() => new Parser('1.234, 5.678', '')).toThrow('options must be an object');
 });
 
+it('throws an error if no parser is found', () => {
+    expect(() => new Parser('unknown format')).toThrow('No parser found for the given coordinate string');
+});
+
 it('returns the correct latitude and longitude for "1.234, 5.678"', () => {
     const parser = new Parser('1.234, 5.678');
     expect(parser.getLatitude()).toBe(1.234);
     expect(parser.getLongitude()).toBe(5.678);
-});
-
-it('throws an error if no parser is found', () => {
-    expect(() => new Parser('unknown format')).toThrow('No parser found for the given coordinate string');
 });
